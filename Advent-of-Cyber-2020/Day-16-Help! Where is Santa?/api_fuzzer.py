deb08d4a68ac91aca21390b8623c1c4e6431d3d6
gilfoyle97 <edoardott@gmail.com>
2020-12-16 17:29:06 +0100

    Update api_fuzzer.py

--- Advent-of-Cyber-2020/Day-16-Help! Where is Santa?/api_fuzzer.py
+++ Advent-of-Cyber-2020/Day-16-Help! Where is Santa?/api_fuzzer.py
@@ -1,7 +1,9 @@
 import requests
 
+TARGET_API = "HERE YOUR TARGET_IP"
+
 for i in range(0, 100):
     if i %2 == 1:
-        response = requests.get('http://10.10.166.237:8000/api/{}'.format(str(i)))
+        response = requests.get('http://' + TARGET_API + ':8000/api/{}'.format(str(i)))
         print(str(i) + " : " + str(response.status_code))
         print(response.text)
